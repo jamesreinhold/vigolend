76880b13f956500ae3688613004beb2d33149c4e
James Reinhold <mrjamesreinhold@gmail.com>
2021-05-28 11:48:37 +0200

    Database Migrations

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'iso2', 'created_date', 'modified_date')
+    list_display_links = ('name', 'iso2', 'created_date', 'modified_date')
+
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'iso2', 'country_code', 'created_date', 'modified_date')
+    list_display_links = ('name', 'iso2', 'country_code', 'created_date', 'modified_date')
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country_code', 'state_code', 'created_date', 'modified_date')
+    list_display_links = ('name', 'country_code', 'state_code', 'created_date', 'modified_date')
